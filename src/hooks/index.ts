b689dd906d3bfd6c5fe5d4f2cf7258e95fe68290
Or1gianld <superhd@icloud.com>
2021-12-25 17:36:31 +0300

    Added useAppSelector with types for all app

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useAppDispatch } from './useAppDispatch';
+export { useAppSelector } from './useAppSelector';
+export { useInput } from './useInput';
+export * from './usePagination';
+export type { UseInputReturnTypeAny } from './useInput';
