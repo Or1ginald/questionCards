928d12c8c0ad2ff87ddb25e2182999a90b0e661a
Or1gianld <superhd@icloud.com>
2021-12-28 00:36:12 +0300

    Created custom button component

--- src/components/common/customButton/CustomButton.tsx
+++ src/components/common/customButton/CustomButton.tsx
@@ -1,4 +1,24 @@
-import { ReturnComponentType } from 'types';
+import React from 'react';
 
-// eslint-disable-next-line jsx-a11y/control-has-associated-label
-export const CustomButton = (): ReturnComponentType => <button />;
+import s from './CustomButton.module.scss';
+
+type ButtonPropsType = {
+  title: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+};
+
+export const CustomButton = React.memo((props: ButtonPropsType) => {
+  const { onClick, title, disabled, type } = props;
+  const onButtonClick = (): void => {
+    if (onClick) {
+      onClick();
+    }
+  };
+  return (
+    <button className={s.button} onClick={onButtonClick} disabled={disabled} type={type}>
+      {title}
+    </button>
+  );
+});
