6e8e81f341d439156d0202897cbb627cd8a1c1c7
Or1gianld <superhd@icloud.com>
2021-12-28 00:44:14 +0300

    Added paths enum

---
+++ src/enum/pathes.ts
@@ -0,0 +1,11 @@
+export enum PATH {
+  REGISTRATION = '/registration',
+  LOGIN = '/login',
+  PROFILE = '/profile',
+  CONFIRM_PASSWORD = '/confirmPassword/*',
+  POPUP = '/popup',
+  CREATE_NEW_PASSWORD = '/createNewPassword/:token',
+  PAGE_404 = '/*',
+  CARDS = '/cards',
+  CARD = '/card',
+}
