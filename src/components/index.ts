fae314c040e4878be88503653a261bc0e6fd6e90
Or1gianld <superhd@icloud.com>
2021-12-28 16:46:52 +0300

    Added index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { Header } from './header';
 export { Profile } from './profile';
 export { RoutesComponent } from './routesComponent';
 export { Login } from './login';
+export { PasswordRecovery } from './passwordRecovery';
 export * from './common';
