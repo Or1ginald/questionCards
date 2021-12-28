e64eae2df4163501c2bfbd3988c91b7ee8b0c8f1
Or1gianld <superhd@icloud.com>
2021-12-28 19:40:54 +0300

    Connected store

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import { appReducer } from './reducers/appReducer';
+import { registrationReducer } from './reducers/registrationReducer';
 
 export const rootReducer = combineReducers({
   app: appReducer,
+  registration: registrationReducer,
 });
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
