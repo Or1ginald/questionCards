80630f93c57b021e449e9bc14f7825aa4069053c
Or1gianld <superhd@icloud.com>
2021-12-28 15:09:04 +0300

    Added index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { Registration } from './registration';
 export { Header } from './header';
 export { Profile } from './profile';
 export { RoutesComponent } from './routesComponent';
+export { Login } from './login';
 export * from './common';
