cc1a17d9ca14624a7cf763a82fc434ac21e3db4c
Or1gianld <superhd@icloud.com>
2022-01-01 17:12:34 +0300

    Added userReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
-import { appReducer, userAuthFormReducer } from 'store/reducers';
+import { appReducer, userAuthFormReducer, userReducer } from 'store/reducers';
 
 export const rootReducer = combineReducers({
   app: appReducer,
   userAuthForm: userAuthFormReducer,
+  user: userReducer,
 });
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
