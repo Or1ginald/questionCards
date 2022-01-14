48513d4f45fc291e8d7cb2c630217bf81d3f7d0e
Or1gianld <superhd@icloud.com>
2022-01-15 02:24:49 +0300

    Added redux devTools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,6 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+/* @ts-ignore */
+
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
 import { appReducer, userAuthFormReducer, userReducer } from 'store/reducers';
@@ -8,7 +10,19 @@ export const rootReducer = combineReducers({
   userAuthForm: userAuthFormReducer,
   user: userReducer,
 });
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+const composeEnhancers =
+  // @ts-ignore
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) ||
+  compose;
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
+
+// export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export type RootStoreType = ReturnType<typeof rootReducer>;
 
