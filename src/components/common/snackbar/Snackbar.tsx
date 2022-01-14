5f3f5465bad0dd3444ecfe4a459fe2619d727ba9
Or1gianld <superhd@icloud.com>
2022-01-15 02:25:20 +0300

    Added autoHide to snackBar

--- src/components/common/snackbar/Snackbar.tsx
+++ src/components/common/snackbar/Snackbar.tsx
@@ -1,13 +1,26 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setErrorAC } from '../../../store/reducers/appReducer';
 
 import s from './SnackBar.module.scss';
 
 import { getError } from 'store';
 
 export const Snackbar = memo(() => {
+  const dispatch = useDispatch();
   const error = useSelector(getError);
+
+  const autoHideDuration = 10000;
+  useEffect(() => {
+    const autoHide = setTimeout(() => {
+      dispatch(setErrorAC(null));
+    }, autoHideDuration);
+    return () => {
+      clearTimeout(autoHide);
+    };
+  });
   return (
     <div className={s.snackbar}>
       <div>{error}</div>
