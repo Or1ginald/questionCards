361e080f1f281b35574b4b0e1db714c524565e50
Or1gianld <superhd@icloud.com>
2022-01-20 14:35:26 +0300

    Created Modal window component

---
+++ src/components/common/modal/Modal.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+
+import cl from './Modal.module.scss';
+
+import { ReturnComponentType } from 'types';
+
+/* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */
+
+export const Modal = (props: ModalPropsType): ReturnComponentType => {
+  const { children, isActive, setIsActive } = props;
+  const rootClasses = [cl.myModal];
+  // const ESCAPE_BUTTON = 27;
+
+  if (isActive) {
+    rootClasses.push(cl.active);
+  }
+
+  const onExitClick = (): void => {
+    setIsActive(false);
+  };
+  // const onEscapePress = (e: KeyboardEvent<HTMLDivElement>): void => {
+  //   if (e.key === 'Escape') {
+  //     setIsActive(false);
+  //   }
+  // };
+
+  useEffect(() => {
+    const close = (e: KeyboardEvent): void => {
+      if (e.key === 'Escape') {
+        setIsActive(false);
+      }
+    };
+    window.addEventListener('keydown', close);
+    return () => window.removeEventListener('keydown', close);
+  }, []);
+
+  return (
+    <div className={rootClasses.join(' ')} onClick={onExitClick}>
+      <div className={cl.myModalContent} onClick={e => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+type ModalPropsType = {
+  isActive: boolean;
+  setIsActive: (isVisible: boolean) => void;
+  children: React.ReactNode;
+};
