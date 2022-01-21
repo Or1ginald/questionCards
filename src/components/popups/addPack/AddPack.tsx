a9aee4108c55eafd3ea6426eafc552f6c5576a59
Or1gianld <superhd@icloud.com>
2022-01-21 17:14:47 +0300

    Added AddPack Component

---
+++ src/components/popups/addPack/AddPack.tsx
@@ -0,0 +1,40 @@
+import React, { memo, useState } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { addPackTC } from '../../../store/reducers/packsReducer';
+
+import st from './addPack.module.scss';
+
+import { CustomButton, CustomTextInput } from 'components';
+
+type AddPackPropsType = {
+  handleModalClose: () => void;
+};
+
+export const AddPack = memo((props: AddPackPropsType) => {
+  const { handleModalClose } = props;
+  const dispatch = useDispatch();
+
+  const [packName, setPackName] = useState('');
+
+  const onAddButtonClick = (): void => {
+    dispatch(addPackTC(packName, handleModalClose));
+  };
+
+  return (
+    <div className={st.addPack}>
+      <h1>Create new Pack</h1>
+      <CustomTextInput
+        placeholder="Pack Name"
+        type="text"
+        onChange={setPackName}
+        value={packName}
+      />
+      <div className={st.buttonsContainer}>
+        <CustomButton title="Cancel" onClick={handleModalClose} />
+        <CustomButton title="Add" onClick={onAddButtonClick} />
+      </div>
+    </div>
+  );
+});
