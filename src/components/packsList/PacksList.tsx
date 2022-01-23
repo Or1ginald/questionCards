da5232964f1a0406700edf8089f9b7a582ba2e39
Or1gianld <superhd@icloud.com>
2022-01-23 19:20:16 +0300

    Refactored component

--- src/components/packsList/PacksList.tsx
+++ src/components/packsList/PacksList.tsx
@@ -1,24 +1,53 @@
-import React, { memo, useEffect } from 'react';
+import React, { KeyboardEvent, memo, useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-import { PATH } from '../../enum';
-import { getCardPacks, getIsAuth } from '../../store';
-import { setPacksTC } from '../../store/reducers/packsReducer';
+import {
+  deletePackTC,
+  setPacksTC,
+  updatePackTC,
+} from '../../store/reducers/packsReducer';
 
-import { Table } from 'components';
+import style from './PacksList.module.scss';
+
+import { TableSidebar, Table, TableNav, Modal, DeletePack, UpdatePack } from 'components';
+import { PATH } from 'enum';
+import { getCardPacks, getIsAuth } from 'store/selectors';
 
 export const PacksList = memo(() => {
   const dispatch = useDispatch();
-
-  // const isLoading = useSelector(getIsLoading);
   const isAuth = useSelector(getIsAuth);
   const cardPacks = useSelector(getCardPacks);
+
+  const [isDeleteModalShown, setIsDeleteModalShown] = useState(false);
+  const [isUpdateModalShown, setIsUpdateModalShown] = useState(false);
+
+  const [packId, setPackId] = useState('');
+
+  const tableHeaders = {
+    name: 'Title',
+    cardsCount: 'Cards',
+    updated: 'Update',
+    user_name: 'Creator',
+  };
 
   useEffect(() => {
     dispatch(setPacksTC());
   }, []);
+
+  const onEscapePress = (e: KeyboardEvent<HTMLDivElement>): void => {
+    if (e.key === 'Escape') {
+      setIsUpdateModalShown(false);
+    }
+  };
+
+  const onDeleteButtonClick = (): void => {
+    dispatch(deletePackTC(packId, () => setIsDeleteModalShown(false)));
+  };
+  const onUpdateButtonClick = (packName: string): void => {
+    dispatch(updatePackTC(packId, packName, () => setIsUpdateModalShown(false)));
+  };
 
   if (!isAuth) {
     return <Navigate to={PATH.LOGIN} />;
@@ -27,5 +56,33 @@ export const PacksList = memo(() => {
   // if (isLoading) {
   //   return <Spinner />;
   // }
-  return <Table cardPacks={cardPacks} />;
+  return (
+    <>
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/no-noninteractive-tabindex */}
+      <div className={style.packsList} onKeyPress={onEscapePress} tabIndex={0}>
+        <TableSidebar />
+        <TableNav />
+        <Table
+          cardPacks={cardPacks}
+          tableHeaders={tableHeaders}
+          // handleDeleteButtonClick={(id: string) => onDeleteButtonClick(id)}
+          setId={setPackId}
+          openDeleteModal={() => setIsDeleteModalShown(true)}
+          openUpdateModal={() => setIsUpdateModalShown(true)}
+        />
+      </div>
+      <Modal isActive={isDeleteModalShown} setIsActive={setIsDeleteModalShown}>
+        <DeletePack
+          handleModalClose={() => setIsDeleteModalShown(false)}
+          onDeleteButtonClick={onDeleteButtonClick}
+        />
+      </Modal>
+      <Modal isActive={isUpdateModalShown} setIsActive={setIsUpdateModalShown}>
+        <UpdatePack
+          handleModalClose={() => setIsUpdateModalShown(false)}
+          handleUpdateButtonPress={onUpdateButtonClick}
+        />
+      </Modal>
+    </>
+  );
 });
