ff0a7eb9f8b1beb4384947f9050861df83f62a66
Or1gianld <superhd@icloud.com>
2022-01-23 19:19:10 +0300

    Refactored Table component

--- src/components/common/table/Table.tsx
+++ src/components/common/table/Table.tsx
@@ -1,73 +1,68 @@
 import React, { memo } from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import { deletePackTC } from '../../../store/reducers/packsReducer';
-import { CustomButton } from '../customButton';
-
 import s from './Table.module.scss';
 
 import { CardPackType } from 'api/types';
-import { getUserId } from 'store/selectors';
-import { ReactComponent as DeleteIcon } from 'svg/delete_black_18dp.svg';
-import { ReactComponent as UpdIcon } from 'svg/edit_black_18dp.svg';
-import { ReactComponent as WatchIcon } from 'svg/visibility_black_18dp.svg';
+import { TableButtons } from 'components';
+import { ARRAY_FIRST_ELEMENT, ARRAY_ZERO_ELEMENT } from 'constants/base';
 
 type TablePropsType = {
   cardPacks: CardPackType[];
+  tableHeaders: { [x: string]: string };
+  // handleDeleteButtonClick: (id: string) => void;
+  setId: (id: string) => void;
+  openDeleteModal: () => void;
+  openUpdateModal: () => void;
 };
 
 export const Table = memo((props: TablePropsType) => {
-  const { cardPacks } = props;
+  const {
+    cardPacks,
+    tableHeaders,
+    // handleDeleteButtonClick,
+    setId,
+    openDeleteModal,
+    openUpdateModal,
+  } = props;
 
-  const dispatch = useDispatch();
-
-  const userProfileId = useSelector(getUserId);
+  // const dispatch = useDispatch();
 
   return (
     <table className={s.table}>
       <thead>
         <tr>
-          <th>Title</th>
-          <th>Cards</th>
-          <th>Updated</th>
-          <th>Creator</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th />
+          {Object.entries(tableHeaders).map(tableHeader => (
+            <th key={tableHeader[ARRAY_ZERO_ELEMENT]}>
+              {tableHeader[ARRAY_FIRST_ELEMENT]}
+            </th>
+          ))}
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
-        {cardPacks.map(
-          ({ name, cardsCount, updated, user_name, _id, more_id }: CardPackType) => {
-            const onDeleteButtonClick = (): void => {
-              dispatch(deletePackTC(_id));
-            };
-            // const onUpdateButtonClick = () => {};
-            return (
-              <tr key={_id}>
-                <td>{name}</td>
-                <td>{cardsCount}</td>
-                <td>{updated}</td>
-                <td>{user_name}</td>
-                <td className={s.flexCell}>
-                  <CustomButton title="Inspect">
-                    <WatchIcon />
-                  </CustomButton>
-                  {more_id === userProfileId && (
-                    <CustomButton title="Update">
-                      <UpdIcon />
-                    </CustomButton>
-                  )}
-                  {more_id === userProfileId && (
-                    <CustomButton title="Delete" onClick={onDeleteButtonClick}>
-                      <DeleteIcon />
-                    </CustomButton>
-                  )}
-                </td>
-              </tr>
-            );
-          },
-        )}
+        {cardPacks.map((el: CardPackType) => (
+          // const onDeleteButtonClick = (): void => {
+          //   dispatch(deletePackTC(el._id));
+          // };
+          // const onUpdateButtonClick = (): void => {
+          //   updatePackTC(_id, name, setIsModalShown);
+          // };
+          <tr key={el._id}>
+            <td>{el.name}</td>
+            <td>{el.cardsCount}</td>
+            <td>{el.updated}</td>
+            <td>{el.user_name}</td>
+            <td className={s.flexCell}>
+              <TableButtons
+                // handleDeleteButtonClick={handleDeleteButtonClick}
+                item={el}
+                setId={() => setId(el._id)}
+                openDeleteModal={openDeleteModal}
+                openUpdateModal={openUpdateModal}
+              />
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
