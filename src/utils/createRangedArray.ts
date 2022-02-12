02bce25e73b16ba5de31f72be0b972583de567c3
Or1gianld <superhd@icloud.com>
2022-02-12 15:37:56 +0300

    fix util

--- src/utils/createRangedArray.ts
+++ src/utils/createRangedArray.ts
@@ -1,6 +1,6 @@
 export const createRangedArray = (start: number, end: number): number[] => {
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  const length = end - start + 1;
+  const one = 1;
+  const length = end - start + one;
   /*
   	Create an array of certain length and set the elements within it from
     start value to end value.
